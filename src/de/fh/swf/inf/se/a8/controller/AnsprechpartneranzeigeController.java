27eafce86c3eb613164d4224bc66364e34fbacf6
disev001 <dsee90@gmail.com>
2016-12-11 14:41:10 +0100

    Anzeige der Details des Ausgewählten Baumelementes

--- src/de/fh/swf/inf/se/a8/controller/AnsprechpartneranzeigeController.java
+++ src/de/fh/swf/inf/se/a8/controller/AnsprechpartneranzeigeController.java
@@ -4,6 +4,8 @@ import de.fh.swf.inf.se.a8.Main;
 import de.fh.swf.inf.se.a8.model.Ansprechpartner;
 import de.fh.swf.inf.se.a8.model.Organisation;
 import de.fh.swf.inf.se.a8.model.TreeViewHelper;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -17,6 +19,16 @@ import java.util.ArrayList;
 public class AnsprechpartneranzeigeController {
     @FXML
     private TreeView<String> tv_AP;
+    @FXML
+    private Label lblAPname;
+    @FXML
+    private Label lblAPmail;
+    @FXML
+    private Label lblAPtel;
+    @FXML
+    private Label lblAPorg;
+    @FXML
+    private Label lblAPansch;
 
     private ObservableList<Ansprechpartner> ansprechpartnerList;
     private ObservableList<Organisation> organisationList;
@@ -27,13 +39,44 @@ public class AnsprechpartneranzeigeController {
     // the initialize method is automatically invoked by the FXMLLoader - it's magic
     @FXML
     public void initialize() {
+        //TODO: Auswahlhandler
 
+        tv_AP.getSelectionModel().selectedItemProperty()
+                .addListener(new ChangeListener<TreeItem<String>>() {
+
+                    @Override
+                    public void changed(
+                            ObservableValue<? extends TreeItem<String>> observable,
+                            TreeItem<String> old_val, TreeItem<String> new_val) {
+                        for (Organisation o : organisationList) {
+                            if (o.getName().equals(new_val.getValue())) {
+
+                                lblAPname.setText("");
+                                lblAPmail.setText("");
+                                lblAPtel.setText("");
+                                lblAPorg.setText(new_val.getValue());
+                                lblAPansch.setText(o.getPlz() + " " + o.getOrt() + "\n" + o.getStrasse());
+                            } else {
+                                for (Ansprechpartner a : ansprechpartnerList) {
+                                    String cache = a.getName() + ", " + a.getVorname();
+                                    if (cache.equals(new_val.getValue())) {
+                                        lblAPname.setText(new_val.getValue());
+                                        lblAPmail.setText(a.getEmail());
+                                        lblAPtel.setText(a.getTelefon());
+                                        lblAPorg.setText(a.getUnternehmen().getName());
+                                        lblAPansch.setText(a.getUnternehmen().getPlz() + " " + a.getUnternehmen().getOrt() + "\n" + a.getUnternehmen().getStrasse());
+                                    }
+                                }
+                            }
+                        }
+                    }
+                });
     }
 
     // loads some strings into the tree in the application UI.
 
     public void loadTreeItems() {
-        TreeViewHelper helper = new TreeViewHelper(ansprechpartnerList,organisationList);
+        TreeViewHelper helper = new TreeViewHelper(ansprechpartnerList, organisationList);
         ArrayList<TreeItem> unternehmen = helper.getUnternehmen();
         TreeItem rootItem = new TreeItem("Unternehmen");
         rootItem.setExpanded(false);
@@ -52,13 +95,14 @@ public class AnsprechpartneranzeigeController {
         this.ansprechpartnerList = this.mainApp.getAnsprechpartners();
         this.organisationList = this.mainApp.getOrganisations();
     }
+
     @FXML
-    public  void handleNewOrg (){
-       this.mainApp.showNewOrg(this.organisationList);
+    public void handleNewOrg() {
+        this.mainApp.showNewOrg(this.organisationList);
     }
 
     @FXML
-    public  void handleNewAnsprechpartner (){
+    public void handleNewAnsprechpartner() {
         this.mainApp.showNewAnsp(this.organisationList);
     }
 }
